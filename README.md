8c21c3ed0102fe96fc79fdd37a730f67787872cc
rbellens <rik.bellens@telin.ugent.be>
2015-12-02 14:21:40 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# moment-import
+
+
+HTML import files for the moment library to load as shared dependencies with link tags.
+
+## Example usage
+
+    <link rel="import" href="../moment-import/moment-min.html">
+    <link rel="import" href="../moment-import/locale/be.html">
+
+
+
---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+mkdir -p locale
+for l in $(ls bower_components/moment/locale)
+ do
+   echo "<script src="../moment/locale/$l"></script>" > "locale/${l/.js/.html}"
+ done
+
+echo "<script src="../moment/moment.js"></script>" > "moment.html"
+echo "<script src="../moment/min/locales.js"></script>" > "locales.html"
+echo "<script src="../moment/min/locales.min.js"></script>" > "locales-min.html"
+echo "<script src="../moment/min/moment.min.js"></script>" > "moment-min.html"
+
